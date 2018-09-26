5448d5cc6c7e86b1e9c52b8f365366e4cfa7f0f3
taisaoko <ttd3calvin@gmail.com>
2018-09-26 17:08:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,9 @@ require 'pry'
 class Song
   attr_accessor :name
   attr_reader :artist
-  extend Memorable, Findable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
   include Paramable
   
   @@songs = []
