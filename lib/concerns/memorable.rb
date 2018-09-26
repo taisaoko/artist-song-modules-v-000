e9b0d1c8f56e1469060e947d7c4a66a382405611
taisaoko <ttd3calvin@gmail.com>
2018-09-26 17:09:53 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,12 @@
 module Memorable
-  def reset_all
-    self.all.clear
-  end
-  
-  def count
-    self.all.count
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+    
+    def count
+      self.all.count
+    end
   end
   
   module InstanceMethods
